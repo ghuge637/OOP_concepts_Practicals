c474244f28b5ff325711c7443ea29c5056ef8ec7
Vishal Ghuge <115221847+ghuge637@users.noreply.github.com>
2026-09-01 12:14:17 +0530

    Update IntrestCalculation.java

--- IntrestCalculation.java
+++ IntrestCalculation.java
@@ -1,3 +1,21 @@
+// Create a class Account with below attributes int id
+// double balance double interestRate
+// Class should have getters, setters and constructor witk parameters in above sequence of attributes
+// create a class Solution with main method. Read one intege and two double values using Scanner object and create object of Account class. These values should be mapped to id, balance and interestRate attributes
+// Read one more integer value and store it in variable noOfYears.
+// Write a method calculatelnterest which will take account object and no of years as input parameters anckreturn the interest amount
+// Consider below logic to calculate the interest value
+// • For specified no of years, first find out the percentage value those no of years based on specified interest Rate, E.g. if no of vears is 5 and specified interestRate is 4%. then 4% of 5 is 0.2. ◦ This percentage should be added to original interstRate for calculating the final interest value, Hence for above example it will be 4.2 (instead of 4)
+// Display the interest amount rounded upto three decima places. Even if the result does not have decimal, it should be displayed with suffix ".000".
+// Consider below sample input and output
+// Input: 1
+// 1000
+// 10 
+// 5
+// Output: 105.000
+// Pr. Pr: pri
+// Output is 55 since the interest rate is 10 and no of years is 5. Hence, the final interest will be 10 + 10 percentage of 5, which ls 10.5, Honco final answer ia 105.000
+
 import java.util.Scanner;
 class Account{
     private int id;
